d89922d640b0784bd0a3424385a6c7f57bd75c7a
Farhat Nader <farhatmnader@gmail.com>
2017-09-03 23:41:08 -0400

    implement log in/out to home page

--- app.py
+++ app.py
@@ -5,6 +5,31 @@ import os
 app = Flask(__name__)
 
 
+@app.route('/')
+def home():
+	if not session.get('logged_in'):
+		return render_template('login.html')
+	else:
+		return render_template('index.html')
+
+
+@app.route('/login', methods=['POST'])
+def login():
+	username = 'admin'
+	password = 'admin'
+
+	if request.form['username'] == username and request.form['password'] == password:
+		session['logged_in'] = True
+	else:
+		flash('Wrong username/password!')
+	return redirect('/')
+
+
+@app.route('/logout', methods=['POST'])
+def logout():
+	session['logged_in'] = False
+	return redirect('/')
+
 
 if __name__ == '__main__':
 	app.secret_key = os.urandom(12)
